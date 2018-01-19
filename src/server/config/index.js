7f89fa8c22018728da7b0241b0ae7d3d6ce80dee
Nilesh <parkhenilesh@gmail.com>
2018-01-19 19:50:21 +0530

    Added Production Mongo link

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,10 +1,11 @@
 export default {
   mongoConnection    : '192.168.0.101:27017/hf',
   testMongoConnection: '192.168.0.101:27017/hf-test',
-  port               : /*process.env.PORT || */3001,
+  port               : 3001,
   hfAddress          : 'https://hentaifox.com',
   localMongo         : 'mongodb://localhost:27017/hf-test',
-  requestHeaders: {
+  prodMongo          : 'mongodb://localhost:27017/hf',
+  requestHeaders     : {
     Connection                 : 'keep-alive',
     'Upgrade-Insecure-Requests': 1,
     'User-Agent'               : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
@@ -13,7 +14,7 @@ export default {
     'Accept-Encoding'          : 'gzip, deflate, sdch, br',
     'Accept-Language'          : 'en-US,en;q=0.8'
   },
-  pagination    : {
+  pagination         : {
     pageSize: 25
   }
 };        
